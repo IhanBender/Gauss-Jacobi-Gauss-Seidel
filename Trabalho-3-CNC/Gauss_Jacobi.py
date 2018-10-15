29146c5f9ea170e0533ffc358c10b86fc60f8a26
IhanBender <ibbender@inf.ufpel.edu.br>
2018-10-14 21:02:41 -0300

    Gauss_Jacobi feito

--- Trabalho-3-CNC/Gauss_Jacobi.py
+++ Trabalho-3-CNC/Gauss_Jacobi.py
@@ -1,25 +1,34 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg  
- 
-# jacobi(np.array([[10, 1], [1, 8]]), np.array([[23], [26]]), np.array([0],[0]), 0.0000001, 1000)
+
+def teste1():
+    return jacobi(np.matrix([[10,1],[1,8]]),    \
+    np.matrix([[23],[26]]),np.matrix([[0],[0]]),0.01, 10)
+
 
 def jacobi(A,b,x0,tol,N):  
     #preliminares  
     A = A.astype('double')  
     b = b.astype('double')  
     x0 = x0.astype('double')  
- 
-    n=np.shape(A)[0]  
+
+    m=np.shape(A)[1]  
+    #test for graphic representation
+    if m != 2:
+        raise NameError('Numero de enquações diferente de dois.') 
+
+    n=np.shape(A)[0]
     x = np.zeros(n)  
     it = 0  
 
-    # Graph
+    #grafico
     ax = plt.axes()
-    xDots = []
-    yDots = []
-    xGap = [0, 0]
-    yGap = [0, 0]
+    x1Dots = [x0[0,0]]
+    x2Dots = [x0[1,0]]
+    x1Gap = [0.0, 0.0]
+    x2Gap = [0.0, 0.0]
     plt.xlabel('x1')
     plt.ylabel('x2')
 
@@ -32,33 +41,48 @@ def jacobi(A,b,x0,tol,N):
             for j in np.concatenate((np.arange(0,i),np.arange(i+1,n))):  
                 x[i] -= A[i,j]*x0[j]  
             x[i] /= A[i,i]  
-        
+
+        #inserting results for graphic        
+        x1Dots.append(x[0])
+        x2Dots.append(x[1])
+
+        #checking if the limits must be extended
+        if x[0] < x1Gap[0]:
+            x1Gap[0] = x[0]
+        elif x[0] > x1Gap[1]:
+            x1Gap[1] = x[0]
+
+        #same for x2
+        if x[1] < x2Gap[0]:
+            x2Gap[0] = x[1]
+        elif x[1] > x2Gap[1]:
+            x2Gap[1] = x[1]
+
         #tolerancia  
-        if (np.linalg.norm(x-x0,np.inf) < tol): 
-            plt.plot(x, (-3 -3*x) / 5.0, x, 2 - 3*x, xDots, yDots, 'bo'
+        if (np.linalg.norm(x-x0,np.inf) < tol):
+            #sets graphic data and returns values 
+            for i in range(0, len(x1Dots) - 1):
+                plt.arrow(x1Dots[i], x2Dots[i],     \
+                         x1Dots[i+1] - x1Dots[i],   \
+                         x2Dots[i+1] - x2Dots[i],   \
+                         head_width=0, head_length=0, width=0.000001,\
+                         fc='k', ec='k', length_includes_head = True)
+
+            plt.xlim(x1Gap[0] - 0.5, x1Gap[1] + 0.5)     # set the x limits
+            plt.ylim(x2Gap[0] - 0.5, x2Gap[1] + 0.5)     # set the y limits
+
+            gx = np.arange(-15, 15, 0.001)
+            plt.plot(gx, (b[0,0] - (A[0,0]*gx)) / A[0,1], gx, (b[1,0] - (A[1,0]*gx)) / A[1,1], x1Dots, x2Dots, 'bo')
             plt.show() 
             return x  
         
         #prepara nova iteracao
         x0 = np.copy(x)
-        #carrega dados para o grafico
-        xDots.append(x0[0])
-        yDots.append(x0[1])
 
     raise NameError('num. max. de iteracoes excedido.') 
 
 
+teste1()
 
-xList = [1,2,3,4]
-yList = [1,4,9,16]
-
-for i in range(0, len(xList) - 1):
-    ax.arrow(xList[i], yList[i], \
-    xList[i+1]-xList[i], yList[i+1]-yList[i], \
-    head_width=0.1, head_length=0.1, fc='k', ec='k', length_includes_head = True)
-
-#ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k', length_includes_head = True)
-
-x = np.arange(0, 17)
 
 
